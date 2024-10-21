16b352c8397566a5bdea6a009d176cef00783cc8
FranciscoDonorbox <francisco.leite@donorbox.org>
2024-10-21 15:07:19 -0300

    a few tests

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,16 @@
 class Product < ApplicationRecord
+  with_options presence: true do
+    validates :title
+    validates :description
+    validates :price
+  end
+
+  validates :title, uniqueness: true
+
+  validates :image_url, allow_blank: true, format: {
+    with: %r{\.(gif|jpg|png)\z}i,
+    message: "must be a URL for GIF, JPG, or PNG image."
+  }
+
+  validates :price, numericality: { greater_than_or_equal_to: 0.01 }
 end
--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -1,7 +1,51 @@
 require "test_helper"
 
 class ProductTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "product attributes must not be empty except for image_url" do
+    product = Product.new
+    attributes = [ :title, :description, :price ]
+
+    assert product.invalid?
+
+    attributes.each do |attribute|
+      assert product.errors[attribute].any?
+    end
+  end
+
+  test "product price must be positive" do
+    product = Product.new(
+      title: "My Book Title",
+      description: "yyy",
+      image_url: "zzz.jpg"
+    )
+
+
+    product.price = 0
+    product.invalid?
+    assert_equal [ "must be greater than or equal to 0.01" ], product.errors[:price]
+
+    product.price = -1
+    product.invalid?
+    assert_equal [ "must be greater than or equal to 0.01" ], product.errors[:price]
+  end
+
+  test "image url must be a URL for GIF, JPG, or PNG image." do
+    product = Product.new(
+      title: "My Book Title",
+      description: "yyy",
+      price: 10.00
+    )
+
+    invalid_urls = %w[
+      text.txt
+      image.pdf
+      image.docx
+    ]
+
+    invalid_urls.each do |invalid_url|
+      product.image_url = invalid_url
+      assert product.invalid?
+      assert_equal [ "must be a URL for GIF, JPG, or PNG image." ], product.errors[:image_url]
+    end
+  end
 end
