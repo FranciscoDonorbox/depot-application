b7901b6717e997d1b8f299490d73a7be9714042e
FranciscoDonorbox <francisco.leite@donorbox.org>
2024-11-07 16:46:24 -0300

    ensuring database level constraints

--- db/migrate/20241018134040_create_products.rb
+++ db/migrate/20241018134040_create_products.rb
@@ -1,12 +1,14 @@
 class CreateProducts < ActiveRecord::Migration[7.2]
   def change
     create_table :products do |t|
-      t.string :title
-      t.text :description
+      t.string :title, null: false
+      t.text :description, null: false
       t.string :image_url
       t.decimal :price, precision: 8, scale: 2
 
       t.timestamps
     end
+
+    add_index :products, :title, unique: true
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -29,12 +29,13 @@ ActiveRecord::Schema[7.2].define(version: 2024_10_26_185848) do
   end
 
   create_table "products", force: :cascade do |t|
-    t.string "title"
-    t.text "description"
+    t.string "title", null: false
+    t.text "description", null: false
     t.string "image_url"
     t.decimal "price", precision: 8, scale: 2
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["title"], name: "index_products_on_title", unique: true
   end
 
   add_foreign_key "line_items", "carts"
